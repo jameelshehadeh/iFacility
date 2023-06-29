1e828099b056956ce176fa4c505631998cca021d
Jameel Shehadeh <jamshehadeh@gmail.com>
2023-06-29 08:42:35 +0400

    Rename ViewController to FacilitiesVC

--- iFacility/SceneDelegate.swift
+++ iFacility/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         let vm = FacilitiesViewModel(networkService: NetworkManager.shared)
-        window.rootViewController = ViewController(viewModel: vm)
+        window.rootViewController = FacilitiesVC(viewModel: vm)
         self.window = window
         window.makeKeyAndVisible()
     }
